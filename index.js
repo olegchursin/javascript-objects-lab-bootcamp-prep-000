17e1cdfdefe98dc82fcf5890b2c16b66e558fe85
Oleg Chursin <olegchursin@gmail.com>
2017-11-07 07:27:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 var recipes = {};
 
-function updateObjectWithKeyAndValue(object, key, value){
+function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, { [key]: value })
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
   return object
+}
+
+function deleteFromObjectByKey(object, key) {
+  var obj = {key: value};
+  var newObj = Object.assign({}, obj);
+  delete newObj.key
+  return obj
 }
